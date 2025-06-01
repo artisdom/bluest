48cd9c0718cb5826cc18189fecc4122c023f33eb
Ted Feng <artisdom@gmail.com>
2025-06-01 17:13:31 +1200

    Add Bluetooth Midi service UUID for "GZUT-MIDI"
    BLE Service
    03b80e5a-ede8-4b33-a751-6ce34ec4c700
    
    BLE Characteristic
    7772e5db-3868-4112-a1a9-f2669d106bf3
    
    const GZUT_PIANO_MIDI_SERVICE: Uuid = Uuid::from_u128(0x03b80e5a_ede8_4b33_a751_6ce34ec4c700);
    const GZUT_PIANO_MIDI_CHARACTERISTIC: Uuid = Uuid::from_u128(0x7772e5db_3868_4112_a1a9_f2669d106bf3);

--- examples/piano.rs
+++ examples/piano.rs
@@ -6,9 +6,8 @@ use futures_lite::{future, StreamExt};
 use tracing::metadata::LevelFilter;
 use tracing::{error, info};
 
-const NORDIC_LED_AND_BUTTON_SERVICE: Uuid = Uuid::from_u128(0x00001523_1212_efde_1523_785feabcd123);
-const BLINKY_BUTTON_STATE_CHARACTERISTIC: Uuid = Uuid::from_u128(0x00001524_1212_efde_1523_785feabcd123);
-const BLINKY_LED_STATE_CHARACTERISTIC: Uuid = Uuid::from_u128(0x00001525_1212_efde_1523_785feabcd123);
+const GZUT_PIANO_MIDI_SERVICE: Uuid = Uuid::from_u128(0x03b80e5a_ede8_4b33_a751_6ce34ec4c700);
+const GZUT_PIANO_MIDI_CHARACTERISTIC: Uuid = Uuid::from_u128(0x7772e5db_3868_4112_a1a9_f2669d106bf3);
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -29,7 +28,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     info!("looking for device");
     let device = adapter
-        .discover_devices(&[NORDIC_LED_AND_BUTTON_SERVICE])
+        .discover_devices(&[GZUT_PIANO_MIDI_SERVICE])
         .await?
         .next()
         .await
@@ -44,62 +43,64 @@ async fn main() -> Result<(), Box<dyn Error>> {
     info!("connected!");
 
     let service = match device
-        .discover_services_with_uuid(NORDIC_LED_AND_BUTTON_SERVICE)
+        .discover_services_with_uuid(GZUT_PIANO_MIDI_SERVICE)
         .await?
         .first()
     {
         Some(service) => service.clone(),
         None => return Err("service not found".into()),
     };
-    info!("found LED and button service");
+    info!("found Piano midi service");
 
     let characteristics = service.characteristics().await?;
     info!("discovered characteristics");
 
-    let button_characteristic = characteristics
+    let midi_characteristic = characteristics
         .iter()
-        .find(|x| x.uuid() == BLINKY_BUTTON_STATE_CHARACTERISTIC)
-        .ok_or("button characteristic not found")?;
+        .find(|x| x.uuid() == GZUT_PIANO_MIDI_CHARACTERISTIC)
+        .ok_or("midi characteristic not found")?;
 
-    let button_fut = async {
-        info!("enabling button notifications");
-        let mut updates = button_characteristic.notify().await?;
-        info!("waiting for button changes");
+    let midi_fut = async {
+        info!("enabling midi notifications");
+        let mut updates = midi_characteristic.notify().await?;
+
+        info!("waiting for midi changes");
         while let Some(val) = updates.next().await {
-            info!("Button state changed: {:?}", val?);
+            info!("Midi state changed: {:?}", val?);
         }
+        info!("finished waiting for midi changes");
         Ok(())
     };
 
     let led_characteristic = characteristics
         .iter()
-        .find(|x| x.uuid() == BLINKY_LED_STATE_CHARACTERISTIC)
+        .find(|x| x.uuid() == GZUT_PIANO_MIDI_CHARACTERISTIC)
         .ok_or("led characteristic not found")?;
 
     let blink_fut = async {
         info!("blinking LED");
         tokio::time::sleep(Duration::from_secs(1)).await;
         loop {
-            led_characteristic.write(&[0x01]).await?;
-            info!("LED on");
+            // led_characteristic.write(&[0x01]).await?;
+            // info!("LED on");
             tokio::time::sleep(Duration::from_secs(1)).await;
-            led_characteristic.write(&[0x00]).await?;
-            info!("LED off");
+            // led_characteristic.write(&[0x00]).await?;
+            // info!("LED off");
             tokio::time::sleep(Duration::from_secs(1)).await;
         }
     };
 
     type R = Result<(), Box<dyn Error>>;
-    let button_fut = async move {
-        let res: R = button_fut.await;
-        error!("Button task exited: {:?}", res);
+    let midi_fut = async move {
+        let res: R = midi_fut.await;
+        error!("Midi task exited: {:?}", res);
     };
     let blink_fut = async move {
         let res: R = blink_fut.await;
         error!("Blink task exited: {:?}", res);
     };
 
-    future::zip(blink_fut, button_fut).await;
+    future::zip(blink_fut, midi_fut).await;
 
     Ok(())
 }
